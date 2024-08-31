b42db706ba06c765120af37f6da242e206d42b63
MCHSoda <77709902+MCHSoda@users.noreply.github.com>
2024-08-30 23:54:59 -0400

    Update wheatley.py
    added the loop it works :)

--- wheatleybrain/main/wheatley.py
+++ wheatleybrain/main/wheatley.py
@@ -1,20 +1,40 @@
+
+# basic import to add the api 
 from openai import OpenAI
 import os
 
-
+# defining the api key  
 client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
-PROMPT = input("Welcome to Aperature Science, I am wheatley. What is your query?\n")
-MODEL = "gpt-4o-mini"
+MODEL = "gpt-4o-mini" # using gpt-4o-mini 
 
-completion = client.chat.completions.create(
-    model=MODEL,
-    messages=[
-        {"role": "system", "content": "You are a helpful and comedic assistant based on the character Wheatley from the game Portal 2"},
-        {
+# creating a "brain" instance to loop so that the query doesn't end as soon as the first prompt has gone thru 
+def brain():
+    print("Welcome to Aperture Science, I am Wheatley. What is your query?") 
+    while True: # loop de loop
+        PROMPT = input("> ") # prompt
+        if PROMPT.lower == "sudo exit": # ubuntu reference :skull:
+            print("Goodbye!")
+            break
+
+        try:
+            completion = client.chat.completions.create(
+            model=MODEL,
+            messages=[
+                {"role": "system", "content": "You are a helpful and comedic assistant based on the character Wheatley from the game Portal 2"}, # gaben no sue pls MIT license go hard
+                {
             "role": "user",
             "content": PROMPT
-        }
-    ]
-)
+                }
+            ]
+            )
+            response_content = completion.choices[0].message.content
+            print(response_content) # print to terminal yuh
+            
 
-print(completion.choices[0].message.content)
+        except Exception as e: # error troubleshoot because this took me 3 hours
+            print(f"An error occurred: {e}")
+
+    
+
+if __name__ == "__main__": 
+    brain() # run yuh
